1fdf3fd613829676704d3be468d3aea030db379c
vitor andrietta <vitorandrietta@gmail.com>
2015-07-13 12:50:43 -0300

    adicao do projeto

---
+++ SeriesTracker/app/src/main/java/com/movile/up/seriestracker/EpisodeDetailsActivity.java
@@ -0,0 +1,17 @@
+package com.movile.up.seriestracker;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class EpisodeDetailsActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_episode_details);
+    }
+
+}
