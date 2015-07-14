4ab32c97d3f385b88321cde3c07ba44d89e0dd49
Vitor Andrietta <vitorandrietta@gmail.com>
2015-07-14 20:53:05 -0300

    correção de bugs de layout de episódios

--- SeriesTracker/app/src/main/java/com/movile/up/seriestracker/EpisodeDetailsActivity.java
+++ SeriesTracker/app/src/main/java/com/movile/up/seriestracker/EpisodeDetailsActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toolbar;
 
 
 public class EpisodeDetailsActivity extends ActionBarActivity {
@@ -12,6 +13,7 @@ public class EpisodeDetailsActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_episode_details);
+
     }
 
 }
