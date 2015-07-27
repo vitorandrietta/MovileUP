5d89fc069d93c8e4240bfb59bd60becda4256000
Vitor Andrietta <vitorandrietta@gmail.com>
2015-07-27 01:31:11 -0300

    navigation drawer layout

---
+++ SeriesTracker/app/src/main/java/com/movile/up/seriestracker/activities/support/BaseNavigationDrawer.java
@@ -0,0 +1,75 @@
+package com.movile.up.seriestracker.activities.support;
+
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.movile.up.seriestracker.R;
+
+public class BaseNavigationDrawer extends BaseLoadingActivity {
+
+    private ViewGroup mRoot;
+    protected DrawerLayout mDrawerLayout;
+    protected ActionBarDrawerToggle mDrawerToggle;
+    protected Toolbar mToolbar;
+
+    @Override
+    public void setContentView(int layoutResID) {
+        LayoutInflater layoutInflater = getLayoutInflater();
+
+        mRoot = (ViewGroup) layoutInflater.inflate(R.layout.activity_base_navigation_drawer, null);
+        ViewGroup content = (ViewGroup) mRoot.findViewById(R.id.base_navigation_content);
+
+        layoutInflater.inflate(layoutResID, content, true);
+        super.setContentView(mRoot);
+    }
+
+    public void configureNavigation() {
+        configureToolbar();
+        configureNavigationDrawer();
+        configureNavigationDrawerContent();
+    }
+
+    private void configureNavigationDrawerContent() {
+        //getSupportFragmentManager().beginTransaction().add(R.id.base_navigation_drawer_content, new FavoritesFragment(), "favorites").commit();
+    }
+
+    private void configureNavigationDrawer() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.base_navigation_drawer_container);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.base_navigation_drawer_open, R.string.base_navigation_drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+    }
+
+    private void configureToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.base_navigation_drawer_toolbar);
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+}
---
+++ SeriesTracker/app/src/main/java/com/movile/up/seriestracker/fragments/FavoritesFragment.java
@@ -0,0 +1,18 @@
+package com.movile.up.seriestracker.fragments;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FavoritesFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
